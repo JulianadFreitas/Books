9c83dc97fba1fedc8f58354371075d595e9e55ca
Rodrigo Martins <rodrigo.martins@ubots.com.br>
2022-07-24 21:39:00 -0300

    Formatting code and organizing imports
    Co-authored-by: Juliana Freitas <julianachristello@gmail.com>

--- components/card.js
+++ components/card.js
@@ -1,15 +1,15 @@
-
-import styles from '../styles/components/Card.module.css'
 import Image from 'next/image'
+import styles from '../styles/components/Card.module.css'
 
-export default function Card({book}) {
-
-return(
-    <a href="https://nextjs.org/docs" className={styles.card}>
-        <h2>{book.title}</h2>
-        <div className={styles.bookImageContainer}><Image src= {book.image} width={100} height={150}></Image></div>
-        <h3>Escrito por {book.author}</h3>
-        <sub>Resumo de {book.summaryAuthor}</sub>
+export default function Card ({ book }) {
+  return (
+    <a href='https://nextjs.org/docs' className={styles.card}>
+      <h2>{book.title}</h2>
+      <div className={styles.bookImageContainer}>
+        <Image src={book.image} width={100} height={150}></Image>
+      </div>
+      <h3>Escrito por {book.author}</h3>
+      <sub>Resumo de {book.summaryAuthor}</sub>
     </a>
-          
-)}
+  )
+}
